1851b426b1bd7c295389c136154b33fb19a0830d
Daniel Puiatti <dpuiatti@oct.ca>
2021-08-31 15:02:50 -0400

    trying to integrate quill editor

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 document.getElementById("submit").addEventListener("click", function() {
-    let postNum = document.querySelector("#num").value;
-    let postTitle = document.querySelector("#title").value;
+    let num = document.querySelector("#num").value;
+    let title = document.querySelector("#title").value;
+    let unit = document.querySelector("#unit").value;
+    let term = document.querySelector("#term").value;
+    let cat = document.querySelector("#cat").value;
+    let desc = document.querySelector("#editor").innerHTML;
+
     let output = document.querySelector("#output");
-    console.log(postNum);
-    output.innerHTML = `
-    <h2> ${postNum} <br/>
-    ${postTitle}</h2>`;
+
+    output.innerHTML =
+        `
+    <h2> 
+    ${num} <br/>
+    ${title}
+    </h2>
+    <h3>
+    ${unit}
+    </h3>
+    <p>
+    <em>
+    ${term} <br/>
+    ${cat}
+    </em>
+    </p>
+    ${desc}
+    `;
 });
