275ceb71e9fcb4dbed8cd4393ac276d2d6cfbfcb
Daniel Puiatti <dpuiatti@oct.ca>
2021-08-31 11:12:33 -0400

    added input fields to form

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let id = document.getElementById("num").value;
-
-function HTMLify() {
-    return console.log(id);
-}
+document.getElementById("submit").addEventListener("click", function() {
+    let postNum = document.querySelector("#num").value;
+    let postTitle = document.querySelector("#title").value;
+    let output = document.querySelector("#output");
+    console.log(postNum);
+    output.innerHTML = `
+    <h2> ${postNum} <br/>
+    ${postTitle}</h2>`;
+});
